6007c110a62b7eb2b60624072e0a4942987f2bb8
danocolombo@google.com <GR0mans1212!>
2021-02-15 07:05:24 -0500

    lobby and header displaying

---
+++ src/pages/lobby/lobby.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Marquee from '../../components/marquee/marquee.component';
+
+import './lobby.styles.scss';
+
+const Lobby = () => (
+    <div className='lobby'>
+        <Marquee />
+    </div>
+);
+
+export default Lobby;
