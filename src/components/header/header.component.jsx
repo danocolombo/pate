6136cef550ef53ccd5765e974f94d3d14a3420dd
danocolombo@google.com <GR0mans1212!>
2021-02-15 13:34:43 -0500

    logo added to navbar

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { TitleContainer } from './header.styles';
 import './header.styles.scss';
+import { ReactComponent as Logo } from '../../assets/pate-logo-white.svg';
 const Header = () => {
     return (
         <div className='header'>
+            <Link className='logo-container' to='/'>
+                <Logo className='logo' />
+            </Link>
             <TitleContainer>JATE</TitleContainer>
         </div>
     );
