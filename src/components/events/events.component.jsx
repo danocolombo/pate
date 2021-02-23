885e9dcbb57614fb4c535beefa4f032504c7ffbd
danocolombo@google.com <GR0mans1212!>
2021-02-22 21:00:40 -0500

    working on events.component to get ActiveEvents

--- src/components/events/events.component.jsx
+++ src/components/events/events.component.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import './events.styles.scss';
-const Events = () => {
-    return (
-        <div>
-            <h3 className='events-header'>Events Here</h3>
-        </div>
-    );
-};
+// ---- notes on doing async await in componentDidMount
+// https://www.valentinog.com/blog/await-react/
+class Events extends React.Component {
+    constructor() {
+        super();
+        this.state = { data: [] };
+    }
+    async componentDidMount() {
+        const response = await fetch(
+            `https://jsonplaceholder.typicode.com/posts`
+        );
+        const json = await response.json();
+        this.setState({ data: json });
+    }
+
+    //     let endPoint = process.env.REACT_APP_PATE_API + '/events';
+    //     endPoint = 'https://jsonplaceholder.typicode.com/posts';
+    //     fetch(`https://jsonplaceholder.typicode.com/posts`)
+    //         .then((res) => res.json())
+    //         .then((json) => this.setState({ events: json }));
+    // }
+    render() {
+        return (
+            <div className='events'>
+                <h2 className='title'>Principle 8 Rally Events</h2>
+                <span>Below you will find the current events</span>
+                <uL>
+                    {this.state.data.map((el) => (
+                        <li>
+                            {el.id}: {el.title}
+                        </li>
+                    ))}
+                </uL>
+            </div>
+        );
+    }
+}
+
 export default Events;
---
+++ src/include/api_headers.js
@@ -0,0 +1,7 @@
+export const api_header_config = {
+    headers: {
+        'Access-Control-Allow-Headers':
+            'Content-Type, x-auth-token, Access-Control-Allow-Headers',
+        'Content-Type': 'application/json',
+    },
+};
