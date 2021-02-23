d8dcc6031c353c4d711bfc902e3b62eb47be1be3
danocolombo <danocolombo@gmail.com>
2021-02-23 07:11:08 -0500

    api CORS work with events.component

--- src/components/events/events.component.jsx
+++ src/components/events/events.component.jsx
@@ -1,39 +1,83 @@
 import React from 'react';
+import axios from 'axios';
 import ReactDom from 'react-dom';
+import { api_header_config } from '../../include/api_headers';
+
 import './events.styles.scss';
 // ---- notes on doing async await in componentDidMount
 // https://www.valentinog.com/blog/await-react/
 class Events extends React.Component {
     constructor() {
         super();
-        this.state = { data: [] };
+        this.state = { data: [], posts: [] };
     }
     async componentDidMount() {
-        const response = await fetch(
-            `https://jsonplaceholder.typicode.com/posts`
-        );
-        const json = await response.json();
-        this.setState({ data: json });
-    }
+        // const res = await fetch(
+        //     `https://jsonplaceholder.typicode.com/posts`
+        // );
+        //------------------------------------
+        let obj = {
+            operation: 'getActiveEvents'
+        };
+        let body = JSON.stringify(obj);
 
-    //     let endPoint = process.env.REACT_APP_PATE_API + '/events';
-    //     endPoint = 'https://jsonplaceholder.typicode.com/posts';
-    //     fetch(`https://jsonplaceholder.typicode.com/posts`)
-    //         .then((res) => res.json())
-    //         .then((json) => this.setState({ events: json }));
-    // }
+        // let api2use = process.env.REACT_APP_PATE_API + '/events';
+        // api2use = 'https://jsonplaceholder.typicode.com/posts';
+        // let res = await axios
+        //     .post(api2use, body, api_header_config)
+        //     .catch((error) => {
+        //         if (error.response) {
+        //             console.log(error.response.data);
+        //             console.log(error.response.status);
+        //             console.log(error.response.headers);
+        //         } else if (error.response) {
+        //             console.log(error.response);
+        //         } else {
+        //             console.log('Error', error.message);
+        //         }
+        //     });
+
+        //this.setState({ data: res });
+    //}
+
+        let endPoint = process.env.REACT_APP_PATE_API + '/events';
+        endPoint = 'https://j7qty6ijwg.execute-api.us-east-1.amazonaws.com/QA/events';
+        //0000000000000000000000000000000000000000000000000000000
+        axios.post(endPoint, body, api_header_config)
+            .then(res => {
+                const posts = res.data;
+            this.setState({ posts });
+        })
+        //0000000000000000000000000000000000000000000000000000000
+
+        //1111111111111111111111111111111111111111111111111111111
+        // endPoint = 'https://jsonplaceholder.typicode.com/posts';
+        // axios.get(endPoint)
+        //     .then(res => {
+        //         const posts = res.data;
+        //     this.setState({ posts });
+        // })
+        //1111111111111111111111111111111111111111111111111111111
+        
+        //2222222222222222222222222222222222222222222222222222222
+        // endPoint = 'https://jsonplaceholder.typicode.com/posts';
+        // fetch(endPoint)
+        //     .then((res) => res.json())
+        //     .then((json) => this.setState({ posts: json }));
+        //2222222222222222222222222222222222222222222222222222222
+    }
     render() {
         return (
             <div className='events'>
                 <h2 className='title'>Principle 8 Rally Events</h2>
                 <span>Below you will find the current events</span>
-                <uL>
-                    {this.state.data.map((el) => (
+                <ul>
+                    {this.state.posts.map((el) => (
                         <li>
                             {el.id}: {el.title}
                         </li>
                     ))}
-                </uL>
+                </ul>
             </div>
         );
     }
--- src/include/api_headers.js
+++ src/include/api_headers.js
@@ -1,7 +1,10 @@
 export const api_header_config = {
     headers: {
         'Access-Control-Allow-Headers':
-            'Content-Type, x-auth-token, Access-Control-Allow-Headers',
+            'Content-Type, X-API-Key, Access-Control-Allow-Headers',
         'Content-Type': 'application/json',
+        'pate-auth': 'b1963g1982',
+        'X-API-Key': 'BgLGv170411z559fo6igTauQ1qx8EFRl7dFlScGH'
     },
 };
+//'Allow-Control-Allow-Headers': 'Content-Type, x-auth-token, Access-Control-Allow-Headers'
