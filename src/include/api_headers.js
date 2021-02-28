43efe3da686ba2f1b03caa112c33d5eac0de825b
danocolombo@google.com <GR0mans1212!>
2021-02-27 19:30:16 -0500

    working on CORS headers

--- src/include/api_headers.js
+++ src/include/api_headers.js
@@ -3,8 +3,9 @@ export const api_header_config = {
         'Access-Control-Allow-Headers':
             'Content-Type, X-API-Key, Access-Control-Allow-Headers',
         'Content-Type': 'application/json',
-        'pate-auth': 'b1963g1982',
-        'X-API-Key': 'BgLGv170411z559fo6igTauQ1qx8EFRl7dFlScGH'
+        Authorization:
+            'AWS4-HMAC-SHA256 Credential=AKIATJIGWGHML7TU3URL/20210223/us-east-1/execute-api/aws4_request, SignedHeaders=host;x-amz-date, Signature=ac280d5762097b7f2bca24fb603d13e059cfd63fa9aea252a797a0bf7a70bf3f',
+        'X-API-Key': 'BgLGv170411z559fo6igTauQ1qx8EFRl7dFlScGH',
     },
 };
 //'Allow-Control-Allow-Headers': 'Content-Type, x-auth-token, Access-Control-Allow-Headers'
