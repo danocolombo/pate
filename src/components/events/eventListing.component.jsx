881c70ddcc1356c6d948c558da0736b383382b24
danocolombo@google.com <GR0mans1212!>
2021-03-01 07:38:28 -0500

    events styling

--- src/components/events/eventListing.component.jsx
+++ src/components/events/eventListing.component.jsx
@@ -24,19 +24,54 @@ const EventListing = ({
     const month2Display = (d) => {
         //get the month, the return String
         let m = d.substring(4, 6);
-        let alpha = mo[m];
-        return alpha;
+        switch (m) {
+            case '01':
+                return 'JAN';
+            case '02':
+                return 'FEB';
+            case '03':
+                return 'MAR';
+            case '04':
+                return 'APR';
+            case '05':
+                return 'MAY';
+            case '06':
+                return 'JUN';
+            case '07':
+                return 'JUL';
+            case '08':
+                return 'AUG';
+            case '09':
+                return 'SEP';
+            case '10':
+                return 'OCT';
+            case '11':
+                return 'NOV';
+            case '12':
+                return 'DEC';
+            default:
+                return 'UNK';
+        }
+    };
+    const day2Display = (d) => {
+        let day = d.substring(6, 8);
+        return day;
     };
     return (
         <Fragment>
-            <div className='event-box'>
+            <div id='event-box'>
                 <div className='date-box'>
-                    <h3 className='event-month'>{month2Display(eventDate)}</h3>
+                    <div className='event-month'>
+                        {month2Display(eventDate)}
+                    </div>
+                    <div className='event-day'>{day2Display(eventDate)}</div>
                 </div>
-                <div className='event-location'>{location.name}</div>
-                <div className='event-street'>{location.street}</div>
-                <div className='event-city'>{location.city}</div>
-                <div className='postal-code'>{location.postalCode}</div>
+                <div className='location-box'>
+                    <div className='event-name'>{location.name}</div>
+                    <div className='event-street'>{location.street}</div>
+                    <div className='event-city'>{location.city}</div>
+                    <div className='postal-code'>{location.postalCode}</div>
+                </div>
             </div>
         </Fragment>
     );
