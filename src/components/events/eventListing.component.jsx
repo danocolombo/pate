dfd8ede521f0c8eaa77f3a865b0d35bc2a3f4be7
danocolombo@google.com <GR0mans1212!>
2021-03-01 17:24:00 -0500

    updated event styles

--- src/components/events/eventListing.component.jsx
+++ src/components/events/eventListing.component.jsx
@@ -6,20 +6,6 @@ const EventListing = ({
     //-------------------------------
     // prep data
     //-------------------------------
-    const mo = {
-        '01': 'JAN',
-        '02': 'FEB',
-        '03': 'MAR',
-        '04': 'APR',
-        '05': 'MAY',
-        '06': 'JUN',
-        '07': 'JUL',
-        '08': 'AUG',
-        '09': 'SEP',
-        10: 'OCT',
-        11: 'NOV',
-        12: 'DEC',
-    };
 
     const month2Display = (d) => {
         //get the month, the return String
@@ -69,8 +55,9 @@ const EventListing = ({
                 <div className='location-box'>
                     <div className='event-name'>{location.name}</div>
                     <div className='event-street'>{location.street}</div>
-                    <div className='event-city'>{location.city}</div>
-                    <div className='postal-code'>{location.postalCode}</div>
+                    <div className='event-city-state-zip'>
+                        {location.city}, {location.state} {location.postalCode}
+                    </div>
                 </div>
             </div>
         </Fragment>
