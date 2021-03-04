d63c639e5ae1150c319d988389dad8b2499b6e0f
danocolombo@google.com <GR0mans1212!>
2021-03-03 20:59:45 -0500

    more cognito work

--- src/actions/cognito.actions.jsx
+++ src/actions/cognito.actions.jsx
@@ -13,86 +13,86 @@ import {
 } from 'amazon-cognito-identity-js';
 import { UserActionTypes } from '../redux/user/user.types';
 import UserPool from './UserPool';
-export const loginWithCognito = (email, password) => async (dispatch) => {
-    const user = new CognitoUser({
-        Username: email,
-        Pool: UserPool,
-    });
+// const loginWithCognito = (email, password) => async (dispatch) => {
+//     const user = new CognitoUser({
+//         Username: email,
+//         Pool: UserPool,
+//     });
 
-    const authDetails = new AuthenticationDetails({
-        Username: email,
-        Password: password,
-    });
-    console.log('email: ' + email);
-    console.log('password: ' + password);
-    console.log('AUTHENTICATE NOW');
-    user.authenticateUser(authDetails, {
-        onSuccess: (data) => {
-            const jwToken = data.idToken.jwtToken;
-            data.token = jwToken;
-            let login_success_data = {
-                token: data.token,
-            };
-            //pass the jwt to LOGIN_SUCCESS
-            dispatch({
-                type: UserActionTypes.LOGIN_SUCCESS,
-                payload: login_success_data,
-            });
-            //======================================
-            // now lets get our user information
-            // from Cognito, send to loadUser
-            let uData = {
-                _id: data.idToken.payload.sub,
-                email: data.idToken.payload.email,
-                firstName: data.idToken.payload.given_name,
-                lastName: data.idToken.payload.family_name,
-                phone: data.idToken.payload.phone_number,
-            };
+//     const authDetails = new AuthenticationDetails({
+//         Username: email,
+//         Password: password,
+//     });
+//     console.log('email: ' + email);
+//     console.log('password: ' + password);
+//     console.log('AUTHENTICATE NOW');
+//     user.authenticateUser(authDetails, {
+//         onSuccess: (data) => {
+//             const jwToken = data.idToken.jwtToken;
+//             data.token = jwToken;
+//             let login_success_data = {
+//                 token: data.token,
+//             };
+//             //pass the jwt to LOGIN_SUCCESS
+//             dispatch({
+//                 type: UserActionTypes.LOGIN_SUCCESS,
+//                 payload: login_success_data,
+//             });
+//             //======================================
+//             // now lets get our user information
+//             // from Cognito, send to loadUser
+//             let uData = {
+//                 _id: data.idToken.payload.sub,
+//                 email: data.idToken.payload.email,
+//                 firstName: data.idToken.payload.given_name,
+//                 lastName: data.idToken.payload.family_name,
+//                 phone: data.idToken.payload.phone_number,
+//             };
 
-            //===================
-            // DANOTEST
-            //===================
-            // try {
-            //     let newUser = {
-            //         login: 'meeter.tester@wmogcolumbus.com',
-            //         password: 'R0mans1212!',
-            //         firstName: 'Meeter',
-            //         lastName: 'Tester',
-            //         phone: '+17066543210',
-            //         email: 'meeter.tester@wmogcolumbus.com',
-            //         role: 'guest', //superuser, owner, manager
-            //         client: 'wbc',
-            //     };
-            //     dispatch(registerUserInPool({ newUser }));
-            // } catch (regError) {
-            //     console.log('registerUserInPool FAILED');
-            // }
-            dispatch({
-                type: UserActionTypes.UPDATE_USER,
-                payload: uData,
-            });
-            // dispatch(loadUser({ uData }));
-        },
-        onFailure: (err) => {
-            console.error('onFailure:', err);
-            const util = require('util');
-            console.log(
-                'err: ' + util.inspect(err, { showHidden: false, depth: null })
-            );
+//             //===================
+//             // DANOTEST
+//             //===================
+//             // try {
+//             //     let newUser = {
+//             //         login: 'meeter.tester@wmogcolumbus.com',
+//             //         password: 'R0mans1212!',
+//             //         firstName: 'Meeter',
+//             //         lastName: 'Tester',
+//             //         phone: '+17066543210',
+//             //         email: 'meeter.tester@wmogcolumbus.com',
+//             //         role: 'guest', //superuser, owner, manager
+//             //         client: 'wbc',
+//             //     };
+//             //     dispatch(registerUserInPool({ newUser }));
+//             // } catch (regError) {
+//             //     console.log('registerUserInPool FAILED');
+//             // }
+//             dispatch({
+//                 type: UserActionTypes.UPDATE_USER,
+//                 payload: uData,
+//             });
+//             // dispatch(loadUser({ uData }));
+//         },
+//         onFailure: (err) => {
+//             console.error('onFailure:', err);
+//             const util = require('util');
+//             console.log(
+//                 'err: ' + util.inspect(err, { showHidden: false, depth: null })
+//             );
 
-            // dispatch(setAlert(err.message, 'danger'));
-            dispatch({
-                type: UserActionTypes.LOGIN_FAILURE,
-            });
-        },
-        // newPasswordRequired: (data) => {
-        //     console.log('newPasswordRequired:', data);
-        // },
-    });
+//             // dispatch(setAlert(err.message, 'danger'));
+//             dispatch({
+//                 type: UserActionTypes.LOGIN_FAILURE,
+//             });
+//         },
+//         // newPasswordRequired: (data) => {
+//         //     console.log('newPasswordRequired:', data);
+//         // },
+//     });
 
-    return null;
-};
-export async function testCognitoFunc(email, password) {
+//     return null;
+// };
+export const testCognitoFunc = (email, password) => async (dispatch) => {
     const user = new CognitoUser({
         Username: email,
         Pool: UserPool,
@@ -128,10 +128,14 @@ export async function testCognitoFunc(email, password) {
                 lastName: data.idToken.payload.family_name,
                 phone: data.idToken.payload.phone_number,
             };
+            console.log('IN ACTION');
             cognitoResponse = data;
             // console.log('_id: ' + data.idToken.payload.sub);
             const util = require('util');
-            console.log('cognito data: ' + util.inspect(data, { showHidden: false, depth: null }));
+            console.log(
+                'cognito data: ' +
+                    util.inspect(data, { showHidden: false, depth: null })
+            );
 
             // console.log('given_name: ' + data.idToken.payload.given_name);
             // console.log('email: ' + data.idToken.password.email);
@@ -191,9 +195,87 @@ export async function testCognitoFunc(email, password) {
         //     console.log('newPasswordRequired:', data);
         // },
     });
-    
+
     // console.log('RETURNING:\n' + JSON.parse(cognitoResponse));
     return cognitoResponse;
     // let msg = 'made it: ' + email;
     // return msg;
 };
+
+const testArrowCognito = (email, password) => async (dispatch) => {
+    const user = new CognitoUser({
+        Username: email,
+        Pool: UserPool,
+    });
+
+    const authDetails = new AuthenticationDetails({
+        Username: email,
+        Password: password,
+    });
+    console.log('email: ' + email);
+    console.log('password: ' + password);
+    console.log('AUTHENTICATE NOW');
+    user.authenticateUser(authDetails, {
+        onSuccess: (data) => {
+            const jwToken = data.idToken.jwtToken;
+            data.token = jwToken;
+            let login_success_data = {
+                token: data.token,
+            };
+            let cognitoResponse = data;
+            // console.log('_id: ' + data.idToken.payload.sub);
+            const util = require('util');
+            console.log(
+                'cognito data: ' +
+                    util.inspect(data, { showHidden: false, depth: null })
+            );
+            //pass the jwt to LOGIN_SUCCESS
+            //@@@@@@@@@@@@@@@@@@@@@@@@@@@
+            //DISPATCH
+            console.log('DISPATCH: login_success_data');
+            // dispatch({
+            //     type: UserActionTypes.LOGIN_SUCCESS,
+            //     payload: login_success_data,
+            // });
+            //======================================
+            // now lets get our user information
+            // from Cognito, send to loadUser
+            // let uData = {
+            //     _id: data.idToken.payload.sub,
+            //     email: data.idToken.payload.email,
+            //     firstName: data.idToken.payload.given_name,
+            //     lastName: data.idToken.payload.family_name,
+            //     phone: data.idToken.payload.phone_number,
+            // };
+
+            //@@@@@@@@@@@@@@@@@@@@@@@@@@
+            //DISPATCH
+            console.log('DISPATCH: UPDATE_USER');
+            // dispatch({
+            //     type: UserActionTypes.UPDATE_USER,
+            //     payload: uData,
+            // });
+        },
+        onFailure: (err) => {
+            console.error('onFailure:', err);
+            const util = require('util');
+            console.log(
+                'err: ' + util.inspect(err, { showHidden: false, depth: null })
+            );
+
+            // dispatch(setAlert(err.message, 'danger'));
+            //@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+            //DISPATCH
+            console.log('LOGIN_FAILURE');
+            // dispatch({
+            //     type: UserActionTypes.LOGIN_FAILURE,
+            // });
+        },
+        // newPasswordRequired: (data) => {
+        //     console.log('newPasswordRequired:', data);
+        // },
+    });
+
+    return null;
+};
+export default testArrowCognito;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,83 +3,91 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {signInWithGoogle} from './google.utils';
-import {loginWithCognito, testCognitoFunc as testIt} from '../../actions/cognito.actions';
+import { signInWithGoogle } from './google.utils';
+import {
+    loginWithCognito,
+    testCognitoFunc,
+} from '../../actions/cognito.actions';
+import testArrowCognito from '../../actions/cognito.actions';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      email: '',
-      password: ''
-    };
-  }
-
-  handleSubmit = async event => {
-    
-    const { email, password } = this.state;
-
-    try {
-      let em = 'danocolombo@gmail.com';
-      let pa = 'R0mans1212!';
-      //await auth.signInWithEmailAndPassword(email, password);
-      console.log('handleSubmit');
-      //await authenticateUser(email, password);
-      // await loginWithCognito(email, password);
-      let uData = await testIt(em, pa);
-      // console.log(msg);
-      const util = require('util');
-      console.log('uData: ' + util.inspect(uData, { showHidden: false, depth: null }));
-
-      this.setState({ email: '', password: '' });
-      console.log('done with handleSubmit');
-    } catch (error) {
-      console.log(error);
+        this.state = {
+            email: '',
+            password: '',
+        };
     }
-  };
 
-  handleChange = event => {
-    const { value, name } = event.target;
+    handleSubmit = async (event) => {
+        const { email, password } = this.state;
 
-    this.setState({ [name]: value });
-  };
+        try {
+            let em = 'danocolombo@gmail.com';
+            let pa = 'R0mans1212!';
+            //await auth.signInWithEmailAndPassword(email, password);
+            console.log('handleSubmit');
+            //await authenticateUser(email, password);
+            // await loginWithCognito(email, password);
+            let uData = await testCognitoFunc(em, pa);
+            // console.log(msg);
+            const util = require('util');
+            console.log(
+                'uData: ' +
+                    util.inspect(uData, { showHidden: false, depth: null })
+            );
 
-  render() {
-    return (
-      <div className='sign-in'>
-        <h2>I already have an account</h2>
-        <span>Sign in with your email and password</span>
+            this.setState({ email: '', password: '' });
+            console.log('done with handleSubmit');
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-        <form onSubmit={this.handleSubmit}>
-          <FormInput
-            name='email'
-            type='email'
-            handleChange={this.handleChange}
-            value={this.state.email}
-            label='email'
-            required
-          />
-          <FormInput
-            name='password'
-            type='password'
-            value={this.state.password}
-            handleChange={this.handleChange}
-            label='password'
-            required
-          />
-          <div className='buttons'>
-            
-            <CustomButton onClick={this.handleSubmit}> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              Google Sign In
-            </CustomButton>
-          </div>
-        </form>
-      </div>
-    );
-  }
+    handleChange = (event) => {
+        const { value, name } = event.target;
+
+        this.setState({ [name]: value });
+    };
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput
+                        name='email'
+                        type='email'
+                        handleChange={this.handleChange}
+                        value={this.state.email}
+                        label='email'
+                        required
+                    />
+                    <FormInput
+                        name='password'
+                        type='password'
+                        value={this.state.password}
+                        handleChange={this.handleChange}
+                        label='password'
+                        required
+                    />
+                    <div className='buttons'>
+                        <CustomButton onClick={this.handleSubmit}>
+                            {' '}
+                            Sign in{' '}
+                        </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            Google Sign In
+                        </CustomButton>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default SignIn;
