48820aab3f950f7d93b97bc6ba3124b96770282f
danocolombo@google.com <GR0mans1212!>
2021-03-08 07:50:03 -0500

    troubleshooting login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import { Switch, Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {
+    Switch,
+    Route,
+    Redirect,
+    BrowserRouter as Router,
+} from 'react-router-dom';
 
 import './App.css';
 import Header from './components/header/header.component';
@@ -12,20 +17,18 @@ import SignIn from './pages/signin/signin.component';
 // import { withAuthenticator } from '@aws-amplify/ui-react';
 import { Auth, authSignInButton } from 'aws-amplify';
 
-
-function App()  {
-    
+function App() {
     const [isLoggedIn, setLoggedIn] = useState(false);
     const checkLoggedInState = () => {
         Auth.currentAuthenticatedUser()
-        .then(sess  => {
-            console.log('logged in');
-            setLoggedIn(true);
-        })
-        .catch(() => {
-            console.log('not logged in');
-            setLoggedIn(false);
-        });
+            .then((sess) => {
+                console.log('logged in');
+                setLoggedIn(true);
+            })
+            .catch(() => {
+                console.log('not logged in');
+                setLoggedIn(false);
+            });
     };
     useEffect(() => {
         checkLoggedInState();
@@ -37,23 +40,22 @@ function App()  {
         } catch (error) {
             console.log('Error logging out:\n:' + error);
         }
-    }
-    
+    };
+
     // render() {
-        return (
-            
-            <Router>
-                <Header loggedIn={isLoggedIn} onClick={signOut}/>
-                <Route exact path='/' component={Lobby} />
-                    {/*  <Route path='/shop' component={ShopPage} />
+    return (
+        <Router>
+            <Header loggedIn={isLoggedIn} onClick={signOut} />
+            <Route exact path='/' component={Lobby} />
+            {/*  <Route path='/shop' component={ShopPage} />
                       // <Route exact path='/checkout' component={CheckoutPage} /> */}
-                <Route exact path='/profile' component={Profile}/>
-                <Route path='/signin' 
-                      render={() => (
-                        <SignIn onLoadEvent={checkLoggedInState}/>
-                      )}/>
-            </Router>
-        );
+            <Route exact path='/profile' component={Profile} />
+            <Route
+                path='/signin'
+                render={() => <SignIn onSignIn={checkLoggedInState} />}
+            />
+        </Router>
+    );
     // }
 }
 
--- src/pages/signin/signin.component.jsx
+++ src/pages/signin/signin.component.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Auth, signInButton } from 'aws-amplify';
 import { Link, useHistory } from 'react-router-dom';
 import FormInput from '../../components/form-input/form-input.component';
@@ -6,57 +6,65 @@ import CustomButton from '../../components/custom-button/custom-button.component
 
 import './signin.styles.scss';
 
-const SignIn = ({onSignIn}) => {
+const SignIn = ({ onSignIn }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const history = useHistory;
+    const history = useHistory();
 
-    const signIn = async () =>{
+    const signIn = async () => {
         try {
             const user = Auth.signIn(username, password);
+            const util = require('util');
+            console.log(
+                'user: ' +
+                    util.inspect(user, {
+                        showHidden: false,
+                        depth: null,
+                    })
+            );
+
             history.push('/');
             onSignIn();
         } catch (error) {
-            console.log('Error signing in' +  error);
+            console.log('Error signing in' + error);
         }
-    }
+    };
     const handleChange = (e) => {
         const { value, name } = e.target;
-        switch(name){
-        case 'username':
-            setUsername(value);
-            break;
-        case 'password':
-            setPassword(value);
-            break;
-        default:
-            break;
-    }
-
-    }
-    return(
+        switch (name) {
+            case 'username':
+                setUsername(value);
+                break;
+            case 'password':
+                setPassword(value);
+                break;
+            default:
+                break;
+        }
+    };
+    return (
         <>
             <FormInput
-            name='username'
-            type='username'
-            handleChange={handleChange}
-            value={username}
-            label='email'
-            required
-        />
-        <FormInput
-            name='password'
-            type='password'
-            value={password}
-            handleChange={handleChange}
-            label='password'
-            required
-        />
-        <div className='buttons'>
-            <CustomButton onClick={signIn}> Sign in </CustomButton>
-        </div>
+                name='username'
+                type='username'
+                handleChange={handleChange}
+                value={username}
+                label='email'
+                required
+            />
+            <FormInput
+                name='password'
+                type='password'
+                value={password}
+                handleChange={handleChange}
+                label='password'
+                required
+            />
+            <div className='buttons'>
+                <CustomButton onClick={signIn}> Sign in </CustomButton>
+            </div>
         </>
-    )
+    );
 };
 
 export default SignIn;
