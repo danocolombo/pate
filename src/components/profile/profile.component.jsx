b7ec75c752780560823525d4266f7d3da87adbc6
danocolombo@google.com <GR0mans1212!>
2021-03-18 08:29:39 -0400

    working on update of profile

--- src/components/profile/profile.component.jsx
+++ src/components/profile/profile.component.jsx
@@ -1,87 +1,201 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import Spinner from '../../components/spinner/Spinner';
 import { setSpinner, clearSpinner } from '../../redux/pate/pate.actions';
 
 import './profile.styles.scss';
-const PersonalProfile = ({currentUser, setSpinner, clearSpinner, pateSystem}) => {
+const PersonalProfile = ({
+    currentUser,
+    setSpinner,
+    clearSpinner,
+    pateSystem,
+}) => {
     // variables for the form
     const [firstName, setFirstName] = useState(currentUser?.firstName);
     const [lastName, setLastName] = useState(currentUser?.lastName);
     const [email, setEmail] = useState(currentUser?.email);
     const [phone, setPhone] = useState(currentUser?.phone);
-    const [homeStreet, setHomeSteet] = useState(currentUser?.address?.street);
+    const [homeStreet, setHomeStreet] = useState(currentUser?.address?.street);
     const [homeCity, setHomeCity] = useState(currentUser?.address?.city);
     const [homeState, setHomeState] = useState(currentUser?.address?.state);
-    const [homePostalCode, setHomePostalCode] = useState(currentUser?.address?.postalCode);
+    const [homePostalCode, setHomePostalCode] = useState(
+        currentUser?.address?.postalCode
+    );
     const [churchName, setChurchName] = useState(currentUser?.church?.name);
     const [churchCity, setChurchCity] = useState(currentUser?.church?.city);
     const [churchState, setChurchState] = useState(currentUser?.address?.state);
-    
-    useEffect(() => {
-        spinnerOn();
-        console.log('LOADED ' + firstName + '\'s profile');
-        
-        spinnerOff();
-        //need to get profile data into state variables
 
-        // effect
-        // return () => {
-        //     cleanup
-        // }
-    }, []);
-    const spinnerOn = () => {
+    useEffect(() => {}, [pateSystem.showSpinner]);
+
+    const handleSubmitClick = (event) => {
+        event.preventDefault();
         setSpinner();
-    }
-    const spinnerOff = () => {
+        // build currentUser object
+        //church values are optional, so we want to send empty string if undefined
+        if (churchName === undefined) setChurchName('');
+        if (churchCity === undefined) setChurchCity('');
+        if (churchState === undefined) setChurchState('');
+        const profileUpdate = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            phone: phone,
+            address: {
+                street: homeStreet,
+                city: homeCity,
+                state: homeState,
+                postalCode: homePostalCode,
+            },
+            church: {
+                name: churchName,
+                city: churchCity,
+                state: churchState,
+            },
+        };
+        const util = require('util');
+        console.log(
+            'profileUpdate: ' +
+                util.inspect(profileUpdate, { showHidden: false, depth: null })
+        );
+
         clearSpinner();
-    }
-    return (
+    };
+    const handleChange = (e) => {
+        const { value, name } = e.target;
+        switch (name) {
+            case 'firstName':
+                setFirstName(value);
+                break;
+            case 'lastNaem':
+                setLastName(value);
+                break;
+            case 'email':
+                setEmail(value);
+                break;
+            case 'phone':
+                setPhone(value);
+                break;
+            case 'homeStreet':
+                setHomeStreet(value);
+                break;
+            case 'homeCity':
+                setHomeCity(value);
+                break;
+            case 'homeState':
+                setHomeState(value);
+                break;
+            case 'homePostalCode':
+                setHomePostalCode(value);
+                break;
+            case 'churchName':
+                setChurchName(value);
+                break;
+            case 'churchCity':
+                setChurchCity(value);
+                break;
+            case 'churchState':
+                setChurchState(value);
+                break;
+            default:
+                break;
+        }
+    };
+
+    return pateSystem.showSpinner ? (
+        <Spinner />
+    ) : (
         <>
             <div className='personalprofilewrapper'>
                 <div className='profileform'>
                     <form>
                         <div>
-                            <label for='first-name'>First name</label>
-                            <input type='text' id='firstname' value={firstName} required />
+                            <label htmlFor='firstName'>First name</label>
+                            <input
+                                type='text'
+                                name='firstName'
+                                id='firstName'
+                                value={firstName}
+                                onChange={handleChange}
+                                required
+                            />
                         </div>
                         <div>
-                            <label for='last-name'>Last name</label>
-                            <input type='text' id='lastname' value={lastName} required />
+                            <label htmlFor='lastName'>Last name</label>
+                            <input
+                                type='text'
+                                id='lastName'
+                                name='lastName'
+                                onChange={handleChange}
+                                value={lastName}
+                                required
+                            />
                         </div>
                         <div>
-                            <label for='profileemail'>E-mail</label>
-                            <input type='text' id='profileemail' value={email} required />
+                            <label htmlFor='email'>E-mail</label>
+                            <input
+                                type='text'
+                                id='email'
+                                name='email'
+                                onChange={handleChange}
+                                value={email}
+                                required
+                            />
                         </div>
                         <div>
-                            <label for='profilephone'>Telephone</label>
-                            <input type='text' id='profilephone' value={phone} required />
+                            <label htmlFor='phone'>Telephone</label>
+                            <input
+                                type='text'
+                                id='phone'
+                                name='phone'
+                                onChange={handleChange}
+                                value={phone}
+                                required
+                            />
                         </div>
                         <div className='profilehomesection'>Home Address</div>
                         <div className='profileaddress'>
                             <div>
-                                <label for='profilestreet'>Street</label>
+                                <label htmlFor='homeStreet'>Street</label>
                                 <input
                                     type='text'
-                                    id='profilestreet'
+                                    id='homeStreet'
+                                    name='homeStreet'
+                                    onChange={handleChange}
                                     value={homeStreet}
                                     required
                                 />
                             </div>
                             <div>
-                                <label for='profilecity'>City</label>
-                                <input type='text' id='profilecity' value={homeCity} required />
+                                <label htmlFor='homeCity'>City</label>
+                                <input
+                                    type='text'
+                                    id='homeCity'
+                                    name='homeCity'
+                                    onChange={handleChange}
+                                    value={homeCity}
+                                    required
+                                />
                             </div>
                             <div>
-                                <label for='profilestate'>State</label>
-                                <input type='text' id='profilestate' value={homeState} required />
+                                <label htmlFor='homeState'>State</label>
+                                <input
+                                    type='text'
+                                    id='homeState'
+                                    name='homeState'
+                                    onChange={handleChange}
+                                    value={homeState}
+                                    required
+                                />
                             </div>
                             <div>
-                                <label for='profilepostalcode'>
+                                <label htmlFor='homePostalCode'>
                                     Postal Code
                                 </label>
                                 <input
                                     type='text'
-                                    id='profilepostalcode'
+                                    id='homePostalCode'
+                                    name='homePostalCode'
+                                    onChange={handleChange}
                                     value={homePostalCode}
                                     required
                                 />
@@ -90,20 +204,38 @@ const PersonalProfile = ({currentUser, setSpinner, clearSpinner, pateSystem}) =>
                                 CR / Church Information
                             </div>
                             <div>
-                                <label for='churchname'>Name</label>
-                                <input type='text' id='churchname' value={churchName} required />
+                                <label htmlFor='churchName'>Name</label>
+                                <input
+                                    type='text'
+                                    id='churchName'
+                                    name='churchName'
+                                    onChange={handleChange}
+                                    value={churchName}
+                                />
                             </div>
                             <div>
-                                <label for='churchcity'>City</label>
-                                <input type='text' id='churchcity' value={churchCity} required />
+                                <label htmlFor='churchCity'>City</label>
+                                <input
+                                    type='text'
+                                    id='churchCity'
+                                    name='churchCity'
+                                    onChange={handleChange}
+                                    value={churchCity}
+                                />
                             </div>
                             <div>
-                                <label for='churchstate'>State</label>
-                                <input type='text' id='churchstate' value={churchState} required />
+                                <label htmlFor='churchState'>State</label>
+                                <input
+                                    type='text'
+                                    id='churchState'
+                                    name='churchState'
+                                    onChange={handleChange}
+                                    value={churchState}
+                                />
                             </div>
                         </div>
-                        <div className='profilebutton'>
-                            <button>UPDATE</button>
+                        <div className='updateButton'>
+                            <button onClick={handleSubmitClick}>UPDATE</button>
                         </div>
                     </form>
                 </div>
@@ -119,7 +251,8 @@ const mapStateToProps = (state) => ({
     currentUser: state.user.currentUser,
     pateSystem: state.pate,
 });
-export default 
-    connect(mapStateToProps, { setSpinner, clearSpinner,mapDispatchToProps })
-(PersonalProfile);
-
+export default connect(mapStateToProps, {
+    setSpinner,
+    clearSpinner,
+    mapDispatchToProps,
+})(PersonalProfile);
