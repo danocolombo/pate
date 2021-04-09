94b273632d2be30c5ebe77d2522588d0f8fcf9f7
danocolombo@google.com <GR0mans1212!>
2021-04-09 08:29:49 -0400

    faq added

--- src/pages/support/support.page.jsx
+++ src/pages/support/support.page.jsx
@@ -3,14 +3,24 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { withRouter } from 'react-router';
 import Header from '../../components/header/header.component';
+import './support.styles.scss';
 const Support = () => {
     return (
         <>
             <Header />
-            <div className="support-box">
-                <div className="support-flex-box__container">
-                    <div className="support-flex-box__body">
-                    Support Data Here
+            <div className='support-box'>
+                <div className='support-flex-box__container'>
+                    <div className='support-flex-box__body'>
+                        <h2>Support</h2>
+                        <p>
+                            If you unfortunately run into issues using the
+                            system, please drop an email and describe your
+                            situation.
+                            <br />
+                            <a href='mailto:fortsonguru@gmail.com'>
+                                pate.support@gmail.com
+                            </a>
+                        </p>
                     </div>
                 </div>
             </div>
