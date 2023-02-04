134a69ec295f59d4673392fdc8710146c0e02c54
danocolombo@google.com <GR0mans1212!>
2023-02-04 12:10:12 -0500

    processing user conversions, but running into throttling challenges

--- src/components/admin/admin-profile-check-list.component.jsx
+++ src/components/admin/admin-profile-check-list.component.jsx
@@ -10,11 +10,52 @@ import * as mutations from '../../graphql/mutations';
 import './admin-registered-users.style.scss';
 import { printObject } from '../../utils/helpers';
 const ProfileDetails = ({ user, pate, clearTmpUser }) => {
+    async function stall(ms) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, ms);
+        });
+    }
     const handleClick = async () => {
         console.log('Clicked: ', user.firstName);
-        printObject('APCLC:14==>user:\n', user);
+        printObject('APCLC:14==>DDBuser:\n', user);
 
         try {
+            //*     create the residence entry
+            // first check if there is any residence to add...
+            let residence = false;
+            if (user?.residence?.street.length > 0) {
+                residence = true;
+            } else if (user?.residence?.city.length > 0) {
+                residence = true;
+            } else if (user?.residence?.stateProv.length > 0) {
+                residence = true;
+            } else if (user?.residence?.postalCode.length > 0) {
+                residence = true;
+            } else if (user?.residence?.latitude?.length > 0) {
+                residence = true;
+            } else if (user?.residence?.longitude?.length > 0) {
+                residence = true;
+            }
+            let residenceId = '';
+            if (residence === true) {
+                const resDef = {
+                    street: user?.residence?.street || '',
+                    city: user?.residence?.city || '',
+                    stateProv: user?.residence?.stateProv || '',
+                    postalCode: parseInt(user?.residence?.postalCode) || null,
+                };
+                printObject('APCLC:25==>resDef:\n', resDef);
+
+                const resResults = await API.graphql({
+                    query: mutations.createResidence,
+                    variables: { input: resDef },
+                });
+                console.log('APCLC:48-->resResults:\n', resResults);
+                residenceId = resResults.data.createResidence.id;
+                //      STALL 1 second to prevent throttling
+                await stall(1000);
+            }
+            //      createUser
             const userDef = {
                 sub: user.uid,
                 username: user?.username || '',
@@ -22,22 +63,33 @@ const ProfileDetails = ({ user, pate, clearTmpUser }) => {
                 lastName: user?.lastName || '',
                 email: user?.email || '',
                 phone: user?.phone || '',
+                residenceResidentsId: residenceId,
                 divisionDefaultUsersId: 'fffedde6-5d5a-46f0-a3ac-882a350edc64',
-                // street: user?.residence?.street || '',
-                // city: user?.residence?.city || '',
-                // stateProv: user?.residence?.stateProv || '',
-                // postalCode: parseInt(user?.residence?.postalCode) || null,
             };
-            printObject('APCLC:31==>userDef:\n', userDef);
-
-            const results = await API.graphql({
+            printObject('APCLC:62==>userDef:\n', userDef);
+            const userResults = await API.graphql({
                 query: mutations.createUser,
                 variables: { input: userDef },
             });
-            console.log('APCLC:37-->results:\n', results);
-            //* now create the residence entry
+            console.log('APCLC:68-->userResults:\n', userResults);
+            const userRefId = userResults.data.createUser.id;
+            //      STALL 1 second to prevent throttling
+            await stall(3000);
+            //      createAffiliation
+            const affDef = {
+                role: user?.role || 'guest',
+                status: 'active',
+                userAffiliationsId: userRefId,
+                divisionAffiliationsId: 'fffedde6-5d5a-46f0-a3ac-882a350edc64',
+            };
+            printObject('APCLC:77==>affDef:\n', affDef);
+            const affResults = await API.graphql({
+                query: mutations.createAffiliation,
+                variables: { input: affDef },
+            });
+            console.log('APCLC:83-->affResults:\n', affResults);
         } catch (error) {
-            console.log('APCLC:39-->error:', error);
+            console.log('APCLC:85-->error:', error);
         }
     };
     useEffect(() => {}, []);
