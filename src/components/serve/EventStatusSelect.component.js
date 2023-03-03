23503604646b1b0ac2426fbe82e85a0f4d16989e
danocolombo@google.com <GR0mans1212!>
2023-03-03 10:25:54 -0500

    working on serve event modify error handling

--- src/components/serve/EventStatusSelect.component.js
+++ src/components/serve/EventStatusSelect.component.js
@@ -1,7 +1,14 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import React, { useState } from 'react';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 
-export default function EventStatusSelect({ value, onChange }) {
-  /*
+export default function EventStatusSelect({
+    value,
+    onChange,
+    initialValue,
+    role,
+}) {
+    const [selectedValueError, setSelectedValueError] = useState('');
+    /*
         The state transitions are as follows
         state               options
         -----------------------------------------------------
@@ -11,68 +18,75 @@ export default function EventStatusSelect({ value, onChange }) {
         approved            draft | cancel | 
         done                draft
     */
-  const eventOptions = [];
-  switch (value) {
-    case "draft":
-      eventOptions.push({ id: "draft", name: "Draft" });
-      eventOptions.push({ id: "review", name: "Ready for Review" });
-      eventOptions.push({ id: "cancel", name: "Cancel" });
-      eventOptions.push({ id: "delete", name: "Delete" });
-      break;
-    case "review":
-      eventOptions.push({ id: "review", name: "In review" });
-      eventOptions.push({ id: "draft", name: "Return to Coordinator" });
-      eventOptions.push({ id: "approved", name: "Approve" });
-      eventOptions.push({ id: "cancel", name: "Cancel" });
-      eventOptions.push({ id: "delete", name: "Delete" });
-      break;
-    case "approved":
-      eventOptions.push({ id: "approved", name: "Approved" });
-      eventOptions.push({ id: "draft", name: "Return to Coordinator" });
-      eventOptions.push({ id: "review", name: "Back to Review" });
-      eventOptions.push({ id: "cancel", name: "Cancel" });
-      eventOptions.push({ id: "delete", name: "Delete" });
-      break;
-    case "cancel":
-      eventOptions.push({ id: "cancel", name: "Cancelled" });
-      eventOptions.push({ id: "draft", name: "Back to Draft" });
-      eventOptions.push({ id: "delete", name: "Delete" });
-      break;
-    case "delete":
-      eventOptions.push({ id: "delete", name: "Deleted" });
-      eventOptions.push({ id: "draft", name: "Undelete to Draft" });
-      break;
+    const eventOptions = [];
+    switch (initialValue) {
+        case 'draft':
+            eventOptions.push({ id: 'draft', name: 'Draft' });
+            eventOptions.push({ id: 'review', name: 'Ready for Review' });
+            eventOptions.push({ id: 'cancel', name: 'Cancel' });
+            eventOptions.push({ id: 'delete', name: 'Delete' });
+            break;
+        case 'review':
+            eventOptions.push({ id: 'review', name: 'In review' });
+            eventOptions.push({ id: 'draft', name: 'Return to Coordinator' });
+            if (role === 'lead' || role === 'director' || role === 'guru') {
+                eventOptions.push({ id: 'approved', name: 'Approve' });
+            }
+            eventOptions.push({ id: 'cancel', name: 'Cancel' });
+            eventOptions.push({ id: 'delete', name: 'Delete' });
+            break;
+        case 'approved':
+            eventOptions.push({ id: 'approved', name: 'Approved' });
+            eventOptions.push({ id: 'draft', name: 'Return to Coordinator' });
+            eventOptions.push({ id: 'review', name: 'Back to Review' });
+            eventOptions.push({ id: 'cancel', name: 'Cancel' });
+            eventOptions.push({ id: 'delete', name: 'Delete' });
+            break;
+        case 'cancel':
+            eventOptions.push({ id: 'cancel', name: 'Cancelled' });
+            eventOptions.push({ id: 'draft', name: 'Back to Draft' });
+            eventOptions.push({ id: 'delete', name: 'Delete' });
+            break;
+        case 'delete':
+            eventOptions.push({ id: 'delete', name: 'Deleted' });
+            eventOptions.push({ id: 'draft', name: 'Undelete to Draft' });
+            break;
 
-    default:
-      break;
-  }
+        default:
+            break;
+    }
 
-  return (
-    <>
-      <InputLabel id="meeting-state-select-label">Event Status</InputLabel>
-      <Select
-        labelId="meeting-state-select-label"
-        id="meeting-state-select"
-        style={{ padding: "0px", margin: "0px" }}
-        value={value}
-        onChange={onChange}
-        sx={{
-          "& .Mui-selected.Mui-select-approved": {
-            color: "green",
-            backgroundColor: "green",
-          },
-          "& .Mui-selected.Mui-select-rejected": {
-            color: "white",
-            backgroundColor: "red",
-          },
-        }}
-      >
-        {eventOptions.map((option) => (
-          <MenuItem key={option.id} value={option.id}>
-            {option.name}
-          </MenuItem>
-        ))}
-      </Select>
-    </>
-  );
+    return (
+        <>
+            <InputLabel id='meeting-state-select-label'>
+                Event Status
+            </InputLabel>
+            <Select
+                labelId='meeting-state-select-label'
+                id='meeting-state-select'
+                style={{ padding: '0px', margin: '0px' }}
+                value={value}
+                onChange={(event) => {
+                    const value = event.target.value;
+                    onChange(value);
+                }}
+                sx={{
+                    '& .Mui-selected.Mui-select-approved': {
+                        color: 'green',
+                        backgroundColor: 'green',
+                    },
+                    '& .Mui-selected.Mui-select-rejected': {
+                        color: 'white',
+                        backgroundColor: 'red',
+                    },
+                }}
+            >
+                {eventOptions.map((option) => (
+                    <MenuItem key={option.id} value={option.id}>
+                        {option.name}
+                    </MenuItem>
+                ))}
+            </Select>
+        </>
+    );
 }
